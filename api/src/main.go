1a7ebbf313f49fa04619988ecac6277eaf305814
Beatriz Ferrante <beatriz.ferrante37@gmail.com>
2024-09-16 22:05:34 -0300

    Fazendo conexão com a api openweathermap

--- api/src/main.go
+++ api/src/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"net/http"
 	"os"
+	"strings"
 )
 
 type apiConfigData struct {
@@ -27,4 +29,41 @@ func configApi(filename string) (apiConfigData, error) {
 	}
 
 	return api, nil
+}
+
+func main() {
+	http.HandleFunc("/clima/",
+		func(w http.ResponseWriter, r *http.Request) {
+			cidade := strings.SplitN(r.URL.Path, "/", 3)[2]
+			data, err := consulta(cidade)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(data)
+		})
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func consulta(cidade string) (weatherData, error) {
+	apiConfig, err := configApi(".apiConfig")
+	if err != nil {
+		return weatherData{}, err
+	}
+
+	resp, err := http.Get("https://api.openweathermap.org/data/2.5/weather?appid={API key}" + apiConfig.ApiKey + "&q=" + cidade + "&units=metric")
+	if err != nil {
+		return weatherData{}, err
+	}
+
+	defer resp.Body.Close()
+
+	var d weatherData
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return weatherData{}, err
+	}
+	return d, nil
 }

370628eef44b373ea0efffbc0e71109ca98c7e2c
Beatriz Ferrante <beatriz.ferrante37@gmail.com>
2024-09-16 21:12:59 -0300

    Criando estrutura e configuração da api

--- api/src/main.go
+++ api/src/main.go
@@ -1,11 +1,30 @@
 package main
 
-const (
-	serviceName = "https://bsky.social"
+import (
+	"encoding/json"
+	"os"
 )
 
-func main() {
-	config := dotenv.Load("config.env")
+type apiConfigData struct {
+	ApiKey string `json:"ApiKeyWeather"`
+}
 
-	serviceName.
+type weatherData struct {
+	Nome string  `json:"nome"`
+	Temp float64 `json:"temp"`
+}
+
+func configApi(filename string) (apiConfigData, error) {
+	bytes, err := os.ReadFile(filename)
+	if err != nil {
+		return apiConfigData{}, err
+	}
+
+	var api apiConfigData
+	err = json.Unmarshal(bytes, &api)
+	if err != nil {
+		return apiConfigData{}, err
+	}
+
+	return api, nil
 }
